fd72ca9813e548fbfbb90912aa3aac6abca24a69
Kevin Sandberg <Kevinsandb3607@student.lvusd.org>
2025-12-14 15:50:09 -0800

    Added all 6 basic trig functions and one special case

--- Trig.py
+++ Trig.py
@@ -59,12 +59,91 @@ class Tan(Function):
         self.inside = inside
 
     def evaluate(self, x):
-        return self.coef*cos(self.inside.evaluate(x))
+        return self.coef*tan(self.inside.evaluate(x))
 
     def to_string(self):
         if not self.coef == 1:
-            return f"[{self.coef}cos({self.inside.to_string()})]"
-        return f"cos({self.inside.to_string()})"
+            return f"[{self.coef}tan({self.inside.to_string()})]"
+        return f"tan({self.inside.to_string()})"
+
+    # add ln support later if you feel like it
+    def take_integral(self):
+        #return Sin(coef=self.coef, inside=self.inside)
+        return None
+
+    def take_derivative(self):
+        #checks if it is just multiplying by 1
+        if self.inside.take_derivative().to_string() == "":
+            return RealExponentFunction(Sec(coef=self.coef, inside=self.inside), 2)
+        return MultipliedFunction(RealExponentFunction(Sec(coef=self.coef, inside=self.inside), 2), self.inside.take_derivative())
+
+class Csc(Function):
+    def __init__(self, inside: Function = Polynomial(1, [1], [1]), coef=1):
+        super().__init__()
+        self.coef = coef
+        self.inside = inside
+
+    def evaluate(self, x):
+        return self.coef/sin(self.inside.evaluate(x))
+
+    def to_string(self):
+        if not self.coef == 1:
+            return f"[{self.coef}csc({self.inside.to_string()})]"
+        return f"csc({self.inside.to_string()})"
+
+    # add ln support later if you feel like it
+    def take_integral(self):
+        #return Sin(coef=self.coef, inside=self.inside)
+        return None
+
+    def take_derivative(self):
+        csc_out = deepcopy(self)
+        # flip sign
+        csc_out.coef *= -1
+        #checks if it is just multiplying by 1
+        if self.inside.take_derivative().to_string() == "":
+            return MultipliedFunction(csc_out, Cot(self.inside))
+        return MultipliedFunction(MultipliedFunction(csc_out, Cot(self.inside)), self.inside.take_derivative())
+class Sec(Function):
+    def __init__(self, inside: Function = Polynomial(1, [1], [1]), coef=1):
+        super().__init__()
+        self.coef = coef
+        self.inside = inside
+
+    def evaluate(self, x):
+        return self.coef/cos(self.inside.evaluate(x))
+
+    def to_string(self):
+        if not self.coef == 1:
+            return f"[{self.coef}sec({self.inside.to_string()})]"
+        return f"sec({self.inside.to_string()})"
+
+    # add ln support later if you feel like it
+    def take_integral(self):
+        #return Sin(coef=self.coef, inside=self.inside)
+        return None
+
+    # returns -cos(u) * du
+    def take_derivative(self):
+        sec_out = deepcopy(self)
+        # checks if it is just multiplying by 1
+        if self.inside.take_derivative().to_string() == "":
+            return MultipliedFunction(sec_out, Tan(self.inside))
+        return MultipliedFunction(MultipliedFunction(sec_out, Tan(self.inside)), self.inside.take_derivative())
+
+class Cot(Function):
+    def __init__(self, inside: Function = Polynomial(1, [1], [1]), coef=1):
+        super().__init__()
+        self.coef = coef
+        self.inside = inside
+
+    def evaluate(self, x):
+        return self.coef*tan(self.inside.evaluate(x))**-1
+
+    def to_string(self):
+        if not self.coef == 1:
+            return f"[{self.coef}cot({self.inside.to_string()})]"
+        return f"cot({self.inside.to_string()})"
 
     # add ln support later if you feel like it
     def take_integral(self):
@@ -75,5 +154,14 @@ class Tan(Function):
     def take_derivative(self):
         #checks if it is just multiplying by 1
         if self.inside.take_derivative().to_string() == "":
-            return RealExponentFunction(Cos(coef=-self.coef, inside=self.inside), -2)
-        return MultipliedFunction(RealExponentFunction(Cos(coef=-self.coef, inside=self.inside), -2), self.inside.take_derivative())
+            return RealExponentFunction(Csc(coef=-self.coef, inside=self.inside), 2)
+        return MultipliedFunction(RealExponentFunction(Csc(coef=-self.coef, inside=self.inside), 2), self.inside.take_derivative())
+
+# area for special / specific classes due to easy integral rules
+class SecSquared(RealExponentFunction):
+    def __init__(self, inside: Function = Polynomial(1, [1], [1])):
+        inside = Sec(inside)
+        super().__init__(inside=inside, exponent=2)
+
+    def take_integral(self):
+        return Tan(inside=self.inside)
--- main.py
+++ main.py
@@ -6,14 +6,10 @@ from Trig import *
 from radiusVolumeRates import SphereRate
 
 a = Polynomial(3)
+b = SecSquared(a)
 #b = Polynomial(2)
 #c = Polynomial(2)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(a.to_string())
-    d = Cos(inside=a, coef=2)
-    e = Tan(inside=a, coef=2)
-    print(d.to_string())
-    print(d.take_derivative().to_string())
-    print(e.to_string())
-    print(e.take_derivative().to_string())
+    print(b.to_string())
+    print(b.take_integral().to_string())
