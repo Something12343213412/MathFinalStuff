2a5ec3a5c261039954117ab708e8fa8f2ff87882
Kevin Sandberg <Kevinsandb3607@student.lvusd.org>
2025-12-14 16:37:43 -0800

    forgot again what I did

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ from radiusVolumeRates import SphereRate
 
 a = Polynomial(3)
 b = CscSquared(a)
-c = Tan(a)
+c = Tan()
 #b = Polynomial(2)
 #c = Polynomial(2)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(b.to_string())
-    print(b.take_integral().to_string())
-    print(c.take_derivative().to_string())
+    print(f"c deriv = {c.take_derivative().to_string()}")
+    d = c.take_derivative()
     print(c.take_derivative().take_integral().to_string())
